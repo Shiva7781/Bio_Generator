6162dc2faae51921d89c469b288f829db9be9a39
Shiva7781 <shivavishwakarma7781@gmail.com>
2022-09-30 22:47:45 +0530

    realtime output added

--- src/components/Display/DisplayData.js
+++ src/components/Display/DisplayData.js
@@ -2,31 +2,61 @@ import React, { useEffect, useState } from "react";
 import "./DisplayData.css";
 
 const DisplayData = ({ entryData, checkBoxes }) => {
-  console.log("entryData:", entryData.school_text);
+  console.log("entryData:", entryData);
   console.log("checkBoxes:", checkBoxes);
 
   const [displayAll, setDisplayAll] = useState();
 
   useEffect(() => {
     setDisplayAll(
-      `${entryData.username} is from ${
-        checkBoxes[0] ? entryData.location_txt : ""
-      }. ${
-        entryData.gender == "Male"
-          ? "He is studying in "
-          : "She is studying in "
-      } ${checkBoxes[1] ? entryData.school_txt : ""} ${entryData.major}`
+      `${
+        checkBoxes[0]
+          ? entryData.username
+          : "You are teaching" + " " + entryData.username + "."
+      }  ${
+        checkBoxes[0] ? "is from" + " " + entryData.location_text + "." : ""
+      } ${
+        checkBoxes[1]
+          ? (entryData.gender === "Male" ? "He" : "She") +
+            " " +
+            "is studying" +
+            " " +
+            entryData.major +
+            " " +
+            "at" +
+            " " +
+            entryData.school_text +
+            "."
+          : ""
+      }  ${
+        checkBoxes[2]
+          ? (entryData.gender === "Male" ? "He" : "She") +
+            " " +
+            "currently works as a" +
+            " " +
+            entryData.occupation_text +
+            "."
+          : ""
+      } ${
+        checkBoxes[3]
+          ? (entryData.gender === "Male" ? "He" : "She") +
+            " " +
+            "was raised " +
+            " " +
+            entryData.religious_text +
+            "."
+          : ""
+      } ${checkBoxes[4] ? entryData.reason_text + "." : ""}`
     );
-  }, [entryData, checkBoxes]);
+  });
 
   return (
     <div className="Display_Data">
       <div>
         <h2>Result</h2>
       </div>
-      <div className="Show">
-        <p>{displayAll}</p>
-      </div>
+
+      <p>{displayAll}</p>
     </div>
   );
 };
--- src/components/Entry/EntryData.js
+++ src/components/Entry/EntryData.js
@@ -5,7 +5,7 @@ import DisplayData from "../Display/DisplayData";
 const EntryData = () => {
   const [location, setLocation] = useState(true);
   const [school, setSchool] = useState(true);
-  const [occupation, setOccupation] = useState(true);
+  const [occupation, setOccupation] = useState(false);
   const [religious, setReligious] = useState(true);
   const [reason, setReason] = useState(true);
 
@@ -13,19 +13,19 @@ const EntryData = () => {
 
   const [entryData, setEntryData] = useState({
     username: "Shiva",
-    gender: "male",
+    gender: "Male",
     location_text: "Nashik",
-    school_text: "K K Wagh",
+    school_text: "K. K. Wagh",
     major: "Poly",
     occupation_text: "MERN",
-    religious_txt: " HINDU",
-    reason_txt: " Enjoy",
+    religious_text: "HINDU",
+    reason_text: "is looking to develop a more personal relationship with God",
   });
 
   const handleInput = (e) => {
     const field = e.target.name;
     const value = e.target.value;
-    // console.log(`Field: ${field}  & Value: ${value}`);
+    console.log(`Field: ${field}  & Value: ${value}`);
 
     setEntryData({ ...entryData, [field]: value });
     // console.log("entryData:", entryData);
@@ -34,21 +34,11 @@ const EntryData = () => {
   const submitForm = (e) => {
     e.preventDefault();
 
-    // console.log("entryData:", entryData);
-
-    // setEntryData({
-    //   username: "",
-    //   gender: "male",
-    //   location_text: "",
-    //   school_text: "",
-    //   major: "",
-    //   occupation_text: "",
-    //   religious_txt: "",
-    //   reason: "",
-    // });
+    console.log("entryData:", entryData);
+    // console.log("entryData:", entryData.location_text);
   };
 
-  const locationCheck = () => {
+  const locationCheck = (e) => {
     location ? setLocation(false) : setLocation(true);
     console.log("location:", location);
   };
@@ -71,181 +61,173 @@ const EntryData = () => {
 
   return (
     <div className="All_Data">
-      <div className="Left_Div">
-        <div className="Entry_Data">
-          <div>
-            <h2>Options</h2>
+      <div className="Entry_Data">
+        <div>
+          <h2>Options</h2>
+        </div>
+
+        <form className="Option_Form" onSubmit={submitForm}>
+          <div className="Form_Div">
+            <label htmlFor="username">Name</label>
+            <input
+              type="text"
+              name="username"
+              value={entryData.username}
+              onChange={handleInput}
+            ></input>
+
+            <label htmlFor="gender">Gender</label>
+            <select name="gender" onChange={handleInput}>
+              <option>Male</option>
+              <option>Female</option>
+            </select>
+            <button onClick={() => console.log("Click")}>Random Name</button>
           </div>
 
-          <form className="Option_Form" onSubmit={submitForm}>
-            <div className="Form_Div">
-              <label htmlFor="username">Name</label>
+          <div className="Form_Div">
+            <label htmlFor="location_text">
               <input
-                type="text"
-                name="username"
-                value={entryData.username}
-                onChange={handleInput}
+                type="checkbox"
+                name="location"
+                checked={location}
+                onChange={locationCheck}
               ></input>
+              Location
+            </label>
+            <input
+              type="text"
+              name="location_text"
+              value={entryData.location_text}
+              onChange={handleInput}
+            ></input>
+            <button onClick={() => console.log("Location")}>
+              Random Location
+            </button>
+          </div>
 
-              <label htmlFor="gender">Gender</label>
-              <select name="gender">
-                <option>Mele</option>
-                <option>Female</option>
-              </select>
-              <button onClick={() => console.log("Click")}>Random Name</button>
-            </div>
-
-            <div className="Form_Div">
-              <label htmlFor="location_text">
-                <input
-                  type="checkbox"
-                  name="location"
-                  checked={location}
-                  onChange={locationCheck}
-                ></input>
-                Location
-              </label>
+          <div className="Form_Div">
+            <label htmlFor="school_text">
               <input
-                type="text"
-                name="location_text"
-                value={entryData.location_text}
-                onChange={handleInput}
+                type="checkbox"
+                name="school"
+                checked={school}
+                onChange={schoolCheck}
               ></input>
-              <button onClick={() => console.log("Location")}>
-                Random Location
+              School
+            </label>
+            <input
+              type="text"
+              name="school_text"
+              value={entryData.school_text}
+              onChange={handleInput}
+            ></input>
+            <button onClick={() => console.log("School")}>Random School</button>
+            <br />
+
+            <label htmlFor="major">Major</label>
+            <input
+              type="text"
+              name="major"
+              value={entryData.major}
+              onChange={handleInput}
+            ></input>
+            <button onClick={() => console.log("Major")}>Random Major</button>
+          </div>
+
+          <div className="Form_Div">
+            <label htmlFor="occupation_text">
+              <input
+                type="checkbox"
+                name="occupation"
+                checked={occupation}
+                onChange={occupationCheck}
+              ></input>
+              Occupation
+            </label>
+            <input
+              type="text"
+              name="occupation_text"
+              value={entryData.occupation_text}
+              onChange={handleInput}
+            ></input>
+
+            <button onClick={() => console.log("Occupation")}>
+              Random Occupation
+            </button>
+          </div>
+
+          <div className="Form_Div">
+            <label htmlFor="religious_text">
+              <input
+                type="checkbox"
+                name="religious"
+                checked={religious}
+                onChange={religiousCheck}
+              ></input>
+              Religious Background
+            </label>
+            <br />
+            <div className="Txt_Btn">
+              <textarea
+                type="text"
+                name="religious_text"
+                value={entryData.religious_text}
+                onChange={handleInput}
+              ></textarea>
+              <br />
+              <button onClick={() => console.log("Religion")}>
+                Random Religion
               </button>
             </div>
+          </div>
 
-            <div className="Form_Div">
-              <label htmlFor="school_text">
-                <input
-                  type="checkbox"
-                  name="school"
-                  checked={school}
-                  onChange={schoolCheck}
-                ></input>
-                School
-              </label>
+          <div className="Form_Div">
+            <label htmlFor="reason_text">
               <input
-                type="text"
-                name="school_text"
-                value={entryData.school_text}
-                onChange={handleInput}
+                type="checkbox"
+                name="reason"
+                checked={reason}
+                onChange={reasonCheck}
               ></input>
-              <button onClick={() => console.log("School")}>
-                Random School
-              </button>
+              Reason for meeting with missionaries
+            </label>
+            <br />
+            <div className="Txt_Btn">
+              <textarea
+                type="text"
+                name="reason_text"
+                value={entryData.reason_text}
+                onChange={handleInput}
+              ></textarea>
               <br />
 
-              <label htmlFor="major">Major</label>
-              <input
-                type="text"
-                name="major"
-                value={entryData.major}
-                onChange={handleInput}
-              ></input>
-              <button onClick={() => console.log("Major")}>Random Major</button>
-            </div>
-
-            <div className="Form_Div">
-              <label htmlFor="occupation_text">
-                <input
-                  type="checkbox"
-                  name="occupation"
-                  checked={occupation}
-                  onChange={occupationCheck}
-                ></input>
-                Occupation
-              </label>
-              <input
-                type="text"
-                name="occupation_text"
-                value={entryData.occupation_text}
-                onChange={handleInput}
-              ></input>
-
-              <button onClick={() => console.log("Occupation")}>
-                Random Occupation
+              <button
+                className="Restoration"
+                onClick={() => console.log("Restoration")}
+              >
+                Restoration
+              </button>
+              <button className="Plan" onClick={() => console.log("Plan")}>
+                Plan of Salvation
+              </button>
+              <button className="Gospel" onClick={() => console.log("Gospel")}>
+                Gospel of Christ
+              </button>
+              <button className="Law" onClick={() => console.log("Law")}>
+                Law of Chastity
+              </button>
+              <button className="Word" onClick={() => console.log("Word")}>
+                Word of Wisdom
+              </button>
+              <button className="Any" onClick={() => console.log("Any")}>
+                Any Lesson
               </button>
             </div>
-
-            <div className="Form_Div">
-              <label htmlFor="religious_txt">
-                <input
-                  type="checkbox"
-                  name="religious"
-                  checked={religious}
-                  onChange={religiousCheck}
-                ></input>
-                Religious Background
-              </label>
-              <br />
-              <div className="Txt_Btn">
-                <textarea
-                  type="text"
-                  name="religious_txt"
-                  value={entryData.religious_txt}
-                  onChange={handleInput}
-                ></textarea>
-                <br />
-                <button onClick={() => console.log("Religion")}>
-                  Random Religion
-                </button>
-              </div>
-            </div>
-
-            <div className="Form_Div">
-              <label htmlFor="reason_txt">
-                <input
-                  type="checkbox"
-                  name="reason"
-                  checked={reason}
-                  onChange={reasonCheck}
-                ></input>
-                Reason for meeting with missionaries
-              </label>
-              <br />
-              <div className="Txt_Btn">
-                <textarea
-                  type="text"
-                  name="reason_txt"
-                  value={entryData.reason_txt}
-                  onChange={handleInput}
-                ></textarea>
-                <br />
-
-                <button
-                  className="Restoration"
-                  onClick={() => console.log("Restoration")}
-                >
-                  Restoration
-                </button>
-                <button className="Plan" onClick={() => console.log("Plan")}>
-                  Plan of Salvation
-                </button>
-                <button
-                  className="Gospel"
-                  onClick={() => console.log("Gospel")}
-                >
-                  Gospel of Christ
-                </button>
-                <button className="Law" onClick={() => console.log("Law")}>
-                  Law of Chastity
-                </button>
-                <button className="Word" onClick={() => console.log("Word")}>
-                  Word of Wisdom
-                </button>
-                <button className="Any" onClick={() => console.log("Any")}>
-                  Any Lesson
-                </button>
-              </div>
-            </div>
-          </form>
-        </div>
+          </div>
+        </form>
       </div>
-      <div className="Right_Div">
-        {<DisplayData entryData={entryData} checkBoxes={checkBoxes} />}
-      </div>
+
+      {<DisplayData entryData={entryData} checkBoxes={checkBoxes} />}
     </div>
   );
 };
